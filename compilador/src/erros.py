e4902aadd89ce9d6758df3c39093bb0fbba14f7f
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2026-02-05 20:27:03 -0300

    implementação inicial da camada de erros

---
+++ compilador/src/erros.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class Posicao:
+    linha: int
+    coluna: int
+
+class ErroCompilador(Exception):
+    def __init__(self, mensagem: str, pos: Posicao | None = None):
+        self.mensagem = mensagem
+        self.pos = pos
+
+        if pos:
+            super().__init__(f"[linha {pos.linha}, coluna {pos.coluna}] {mensagem}")
+        else:
+            super().__init__(mensagem)
+
+
+class ErroLexico(ErroCompilador):
+    pass
