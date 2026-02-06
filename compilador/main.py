6f282fa95efe1e9d35c41637e2ec5180ea89551b
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2026-02-06 00:19:23 -0300

    melhor formatação de output para auxiliar na visualização dos testes

--- compilador/main.py
+++ compilador/main.py
@@ -1,13 +1,24 @@
+from pprint import pprint
+
 from src.lexer import Lexer
+from src.parser import Parser
 from src.erros import ErroCompilador
 
 codigo = """
-inteiro a;
-inteiro b;
-a = 2 + 3 * 4;
-b = (2 + 3) * 4;
-escreva(a);
-escreva(b);
+inteiro x;
+x = 10;
+
+se (x > 10) entao
+  x = x + 1;
+senao
+  x = 0;
+fimse
+
+enquanto (x < 5) faca
+  x = x + 1;
+fimenquanto
+
+escreva(x);
 """
 
 print("\npara o código:\n")
@@ -16,9 +27,15 @@ print(codigo)
 print("=============================\n")
 
 try:
-    tokens = Lexer().tokenizer(codigo)
+    tokens = Lexer().tokenizar(codigo)
+    arvore = Parser(tokens).parse()
 
+    print("------- TOKENS -------")
     for token in tokens:
         print(token)
+
+    print("\n------- AST -------")
+    pprint(arvore)
+
 except ErroCompilador as e:
     print(e)
